273371666fa9f95d173c7600b189c560970bdcff
czkg <czdutss@gmail.com>
2019-05-26 23:06:26 -0400

    5/26

--- src/heatmap2pose.py
+++ src/heatmap2pose.py
@@ -3,6 +3,8 @@ import numpy as np
 import argparse
 from glob import glob
 import tqdm
+from shutil import rmtree
+import scipy.io
 
 
 if __name__ == '__main__':
@@ -10,7 +12,7 @@ if __name__ == '__main__':
 	parser.add_argument('--input_path', type=str, help='the input path')
 	parser.add_argument('--output_path', type=str, help='the output path')
 	parser.add_argument('--dim_heatmap', type=int, default=64, help='the dimension of the heatmap')
-	parser.add_argument('--n_joints', type=int, default=17, delp='the number of joints')
+	parser.add_argument('--n_joints', type=int, default=17, help='the number of joints')
 	args = vars(parser.parse_args())
 
 	input_path = args['input_path']
@@ -18,19 +20,23 @@ if __name__ == '__main__':
 	dim_heatmap = args['dim_heatmap']
 	n_joints = args['n_joints']
 
+	if os.path.exists(output_path):
+		rmtree(output_path)
+	os.makedirs(output_path)
+
 	dim_xy = dim_heatmap ** 2
-	dim = size_xy + dim_heatmap
+	#dim = dim_xy + dim_heatmap
 
 	l = np.linspace(-1, 1, dim_heatmap)
 	file_list = glob(input_path + '/*.mat')
 	num = len(file_list)
 	for i in tqdm.trange(num):
 		f = file_list[i]
-		filename = f.split('/')[-1]
+		filename = f.split('/')[-1][:-4] + '.csv'
 		data = scipy.io.loadmat(f)['heatmap'][0]
 		data = np.split(data, n_joints)
-		data_xy = [d[:size_xy] for d in data]
-		data_z = [d[size_xy:] for d in data]
+		data_xy = [d[:dim_xy] for d in data]
+		data_z = [d[dim_xy:] for d in data]
 
 		xy_max = [np.argmax(dxy) for dxy in data_xy]
 		x_max = [xym // dim_heatmap for xym in xy_max]
@@ -44,4 +50,4 @@ if __name__ == '__main__':
 		pose = pose.T
 
 		name = os.path.join(output_path, filename)
-		scipy.io.savemat(name, {'pose': pose})
+		np.savetxt(name, pose, delimiter = ' ')
